fc1def9b4f58f6b018f52f0ff355154b11b98811
kyle715 <kyle7150@gmail.com>
2018-05-14 17:50:05 -0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var content = new TESTEntities();
+            var post = new Table_1()
+            {
+                Body = "body",
+                DatPulished = DateTime.Now,
+                PostID = 2,
+                Title = "Title"
+            };
+            content.Table_1.Add(post);
+            content.SaveChanges();
+        }
+    }
+}
