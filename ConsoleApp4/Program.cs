d3288706b6a0e32fbc24073188d55c07a230f5cc
kyle715 <kyle7150@gmail.com>
2018-06-11 21:20:35 -0400

    working

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,18 +11,18 @@ namespace ConsoleApp4
     class Program
     {
         static void Main(string[] args)
-        {
-            var hiddenAttributes = new hiddenAttributes();
-            var chromeDriver = new ChromeDriver();
-            chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/?.src=finance&.intl=us&.done=https%3A%2F%2Ffinance.yahoo.com%2Fportfolios%253Fbypass%3Dtrue%3Fbypass%3Dtrue&add=1");
-            chromeDriver.FindElementByXPath("//*[@id=\"login-username\"]").Click();
-            chromeDriver.Keyboard.SendKeys(hiddenAttributes.email);
-            chromeDriver.FindElementByXPath("//*[@id=\"login-signin\"]").Click();
-            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            chromeDriver.FindElementByXPath("//*[@id=\"login-passwd\"]").Click();
-            chromeDriver.Keyboard.SendKeys(hiddenAttributes.pswrd);
-            chromeDriver.Keyboard.SendKeys(Keys.Enter);
-            chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
+        //{
+        //    var hiddenAttributes = new hiddenAttributes();
+        //    var chromeDriver = new ChromeDriver();
+        //    chromeDriver.Navigate().GoToUrl("https://login.yahoo.com/?.src=finance&.intl=us&.done=https%3A%2F%2Ffinance.yahoo.com%2Fportfolios%253Fbypass%3Dtrue%3Fbypass%3Dtrue&add=1");
+        //    chromeDriver.FindElementByXPath("//*[@id=\"login-username\"]").Click();
+        //    chromeDriver.Keyboard.SendKeys(hiddenAttributes.email);
+        //    chromeDriver.FindElementByXPath("//*[@id=\"login-signin\"]").Click();
+        //    chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        //    chromeDriver.FindElementByXPath("//*[@id=\"login-passwd\"]").Click();
+        //    chromeDriver.Keyboard.SendKeys(hiddenAttributes.pswrd);
+        //    chromeDriver.Keyboard.SendKeys(Keys.Enter);
+        //    chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
 
             var lastPrice = chromeDriver.FindElementByXPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[1]/td[2]/span").GetAttribute("textContent");
 
